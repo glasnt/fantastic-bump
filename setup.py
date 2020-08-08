3d7c8506282e3f1aa2727f1f3a1915679a1c17ad
Katie McLaughlin <katie@glasnt.com>
2020-08-08 18:59:29 +1000

    Add versioneer

---
+++ setup.py
@@ -0,0 +1,17 @@
+import re
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+import versioneer
+
+setup(
+    name="fantastic-bump",
+    description="fantastic-bump"
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(), 
+    long_description="fantastic-bump",
+    url="https://github.com/glasnt/fantastic-bump",
+    author="Katie McLaughlin",
+    author_email="katie@glasnt.com",
+    license="New BSD",
+)
